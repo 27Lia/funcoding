3194c4f984581a2660bef07b253d036a4dbf49ba
27Lia <tjs4114@gmail.com>
2023-06-27 02:38:37 +0900

    clock.js add

---
+++ Momentum/js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("h2#clock");
+
+ function getClock() {
+        const date = new Date();
+        const hours = String(date.getHours()).padStart(2, "0");
+        const minutes = String(date.getMinutes()).padStart(2, "0");
+        const seconds = String(date.getSeconds()).padStart(2, "0");
+        clock.innerText = `${hours}:${minutes}:${seconds}`;
+      }
+
+ getClock() // webpage가 로딩되자마자 바로 시간이 보여질수있게함.
+ setInterval(getClock,1000);    
+
+ // .padStart(몇자리로만들지, 어떤값을추가할지)메서드활용
+ 
