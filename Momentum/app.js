f26b1ad82d714c1c22d1308ff7eb9c4f968891e5
김선미 <kimsunmi@gimseonmiui-MacBookPro.local>
2023-06-27 01:51:50 +0900

    Updata app.js(paintGreetings function)

--- Momentum/app.js
+++ Momentum/app.js
@@ -8,21 +8,26 @@ function loginSubmit(event){ // loginForm이 제출될 때 실행되는 함수 l
     event.preventDefault(); // 이벤트 기본동작(새로고침)방지하기위해 호출
     loginForm.classList.add("hidden"); // 함수 실행 후 클래스가 추가됨.
     const username = loginInput.value;
-    localStorage.setItem("username", username); // (저장될 아이템 이름,변수)
-    greeting.innerText = `Hello ${username}`; // username 값을 "username"이라는 키로 로컬 스토리지에 저장
-    greeting.classList.remove("hidden"); // "hidden" 클래스를 제거하는 코드 -> h1태그의 hidden클래스를 삭제해주어야 username을 화면에서 볼수있음. 
+    localStorage.setItem("username", username); // (저장될 아이템 이름,변수)(key,value)
+    paintGreetings(username);
 }
 
-const savedUsername = localStorage.getItem("username");
+// username을 매개변수로 받아 greeting 요소의 텍스트를 수정하고, "hidden" 클래스를 제거하는 함수 결국, h1요소가 보여지게하는 함수!
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove("hidden");
+}
+
+// localStorage에서 "username" key의 value를 가져옴(사용자가 입력한 이름이 값이됨)
+const savedUsername = localStorage.getItem("username");  
 
 // local storage에 있는 유저정보 유무 확인하는 조건문
 if (savedUsername === null) {
   loginForm.classList.remove("hidden") // 처음 화면에서 login-form,greeting 모두 hidden상태였기 때문에 유저값이 null인경우 login-form을 보이게함
   loginForm.addEventListener('submit', loginSubmit) // loginForm에 submit 이벤트 리스너를 추가, 폼이 제출될 때 loginSubmit 함수가 실행
 }
-else {
-  greeting.classList.remove("hidden"); // user값이 있는 경우 hidden 클래스 삭제
-  greeting.innerText = `Hello ${savedUsername}`; // {username}이 아닌 이유는 지금은 localstorage에 있는 "username" 이름을 확인하는거니까.. 
+else { 
+    paintGreetings(savedUsername); 
 }
 
 
@@ -38,5 +43,6 @@ else {
 //  event.preventDefault() 이벤트 기본동작을 방지하기 위해 쓰임.
 // 이벤트 리스너를 추가하고자 하는 요소.addEventListener('이벤트 종류', 함수명) 
 // .remove DOM 요소에서 클래스를 제거하는 메서드
-// .localStorage.setItem(key,값)은 Local Storage에 데이터를 저장하는 메서드, 데이터는 "key"와 "value"의 형태로 저장(개발자도구 어플리케이션창에서 확인가능), 저장된 데이터는 문자열 형태로 저장
+// .localStorage.setItem(key,value)은 Local Storage에 데이터를 저장하는 메서드, 데이터는 "key"와 "value"의 형태로 저장(개발자도구 어플리케이션창에서 확인가능), 저장된 데이터는 문자열 형태로 저장
 // local storage에 유저정보가 있으면 form이 보여지면 안됨 h1요소를 보여주어야함 -> 이미 로그인된거니깐 ! -> 결국 local storage에 유저정보 유무를 확인하는 작업 필요
+// getItem() 메서드를 호출하면 키의 값(value)이 반환됨, 값을 변수에 할당하여 사용할 수 있음.
