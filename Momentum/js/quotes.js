5613e16dc61efcca52d023c8e9023208fa970732
27Lia <tjs4114@gmail.com>
2023-06-28 01:36:17 +0900

    todo.js add

--- Momentum/js/quotes.js
+++ Momentum/js/quotes.js
@@ -1,17 +1,15 @@
-fetch('https://api.qwer.pw/request/helpful_text?apikey=guest')
-  .then(response => response.json())
+fetch('https://api.qwer.pw/request/helpful_text?apikey=guest') // API에 GET 요청을 보냄
+  .then(response => response.json()) // 응답 데이터를 JSON으로 파싱
   .then(data => {
-    const respondObj = data[1];
-    if (respondObj && respondObj.respond) {
-      const respond = respondObj.respond;
+    const respondObj = data[1]; // 응답 데이터에서 index 1의 객체를 가져옴
+    if (respondObj && respondObj.respond) { // 가져온 객체와 해당 객체의 respond 속성이 존재하는지 확인
+      const respond = respondObj.respond; // respond 속성 값을 가져옴
       const quotes = document.getElementById('quotes');
-      quotes.textContent = respond;
+      quotes.textContent = respond; // HTML 요소의 텍스트 내용을 가져온 respond 값으로 설정
     } else {
-      console.error('명언 데이터를 가져오는 데 실패했습니다.');
+      console.error('명언 데이터를 가져오는 데 실패했습니다.'); // 데이터가 없거나 형식이 올바르지 않을 때 에러 메시지를 출력
     }
   })
   .catch(error => {
-    console.error('API 요청 실패:', error);
-    // 에러를 처리하는 추가 작업을 수행할 수 있습니다.
-    // 예를 들어, 사용자에게 오류 메시지를 표시하는 등의 작업을 수행할 수 있습니다.
+    console.error('API 요청 실패:', error); // API 요청이 실패했을 때 에러 메시지를 출력
   });
---
+++ Momentum/js/todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = toDoForm.querySelector("input"); // loginForm 안에서 input 요소를 찾아 loginInput 변수에 할당
+
+let toDos = []; //newtodo가 입력된 후 그 이후 값도 업데이트가 가능하도록 let사용
+
+// toDos array를 localStorage에 넣는것임. 
+function saveTodos() {
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement; // 이벤트가 발생한 요소(button을 가리킴)의 부모 요소인 <li> 요소를 선택하는 코드 (즉 button의 부모인 li를 선택!) 
+    li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //toDo는 parsedToDos 배열의 각각의 item들 즉, toDo.id는 각 아이템들의 id 😞어려워. 이해필요! 
+    saveTodos(); // 업데이트 된 toDos 배열을 로컬 스토리지에 저장
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    li.id = newToDo.id; 
+    const span = document.createElement("span");
+    span.innerText = newToDo.text; 
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span); // li자식으로 span을 넣어줌
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+// ToDoSubmit 할 때 실행되는 함수, 
+function handleToDoSubmit(event){ 
+    event.preventDefault(); // 이벤트 기본동작(새로고침)방지하기위해 호출 
+    const newToDo = toDoInput.value; // input값을 newTodo변수에 할당 2️⃣ 사용자가 입력한 값을 newToDo에 할당하고
+    toDoInput.value = ""; // enter했을 때 input칸이 비워지게함. // 3️⃣ input을 비우고
+    const newToDoObj = { // 새로운 객체 
+        text: newToDo,
+        id: Date.now() //현재 시간의 타임스탬프 값
+      };
+    toDos.push(newToDoObj); // 4️⃣ toDos array에 newToDoObj(사용자가 입력한 값과 고유id가 있는객체)가 push됨
+    paintToDo(newToDoObj); // user 할 일 저장해놓은 값 paintToDo함수로 전달 5️⃣ 화면에 사용자가 입력한 값이 보여지게끔 paintToDo함수에 사용자가 입력한 값을 전달
+    saveTodos(); // 6️⃣ 사용자가 입력한 값을 저장
+}
+
+ toDoForm.addEventListener('submit', handleToDoSubmit); // 1️⃣ 사용자가 form을 submit하면 
+
+ const savedTodos = localStorage.getItem("todos")
+
+ if(savedTodos !== null){
+    const parsedToDos = JSON.parse(savedTodos); // localStorage에 있는 string을 obj 또는 array로 변환 여기서는 array
+    toDos = parsedToDos; // toDos는 parsedToDos 배열과 동일한 배열을 참조함. 즉, toDos 배열에 새로운 요소를 추가하거나 삭제하면 parsedToDos 배열에도 동일한 변경사항이 반영됨
+    parsedToDos.forEach(paintToDo); // parsedToDos 배열의 각 item에 대해 paintToDo 함수를 실행 paintToDo가 콜백 함수가 됨
+;
+ }
+
+
